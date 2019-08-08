e90e7fb8b7da058466eb02cbb5547456c2f1a7bf
Your Name <you@example.com>
2019-08-08 20:21:22 +0530

    complete

---
+++ q1.sh
@@ -0,0 +1,6 @@
+echo "hello, What is your name?"
+read name
+mkdir $name
+touch $name/$name.cpp
+echo "Your folder has been created"
+
---
+++ q2.sh
@@ -0,0 +1,11 @@
+echo "hello please enter your name again"
+read name
+mkdir $name 
+touch $name/"name$"1.cpp
+touch $name/"name$"2.cpp
+touch $name/"name$"3.cpp
+touch $name/"name$"4.cpp
+touch $name/"name$"5.cpp
+echo "your folder has been created"
+echo "your file has been created"
+
